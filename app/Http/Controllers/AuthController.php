c0f318bd5663165fc8dee4cbef76a02fa762ec7d
muhammadricoasmara04 <ricoasmara491@gmail.com>
2025-09-18 16:49:11 +0700

    first laravel api presensi

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email'    => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+
+        $user->tokens()->delete();
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'message' => 'Login success',
+            'user'    => $user,
+            'token'   => $token,
+        ]);
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name'       => 'required|string|max:255',
+            'email'      => 'required|email|unique:users',
+            'password'   => 'required|string|min:6',
+            'division'   => 'nullable|string',
+            'work_entry' => 'nullable',
+            'work_end'   => 'nullable',
+            'image'      => 'nullable|string',
+        ]);
+
+        $user = User::create([
+            'name'       => $request->name,
+            'email'      => $request->email,
+            'password'   => Hash::make($request->password),
+            'role'       => 'user',
+            'division'   => $request->division,
+            'work_entry' => $request->work_entry,
+            'work_end'   => $request->work_end,
+            'image'      => $request->image,
+        ]);
+
+        return response()->json([
+            'message' => 'Akun berhasil diregistrasi',
+            'user'    => $user
+        ],201);
+    }
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json([
+            'message' => 'Logged out successfully'
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AdminController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/me', [AuthController::class, 'me']);
+});
